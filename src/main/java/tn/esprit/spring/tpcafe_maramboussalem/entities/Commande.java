5815c66d0c965ecb3aff443f9b0227d2b95c8c50
maramboussalem <marammaramboussalem@gmail.com>
2025-10-07 11:33:26 +0100

    Les Associations

---
+++ src/main/java/tn/esprit/spring/tpcafe_maramboussalem/entities/Commande.java
@@ -0,0 +1,38 @@
+package tn.esprit.spring.tpcafe_maramboussalem.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "Commande")
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+
+public class Commande {
+    @OneToMany (mappedBy = "commande") List<Detail_Commande> detailCommandes;
+
+    @ManyToOne
+    private Client client;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idCommande;
+
+    private LocalDate dateCommande;
+    private float totalCommande;
+
+    @Enumerated(EnumType.STRING)
+    private StatusCommande statusCommande;
+
+}
---
+++ src/main/java/tn/esprit/spring/tpcafe_maramboussalem/repositories/CommandeRepository.java
@@ -0,0 +1,7 @@
+package tn.esprit.spring.tpcafe_maramboussalem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tn.esprit.spring.tpcafe_maramboussalem.entities.Commande;
+
+public interface CommandeRepository extends JpaRepository<Commande, Long> {
+}

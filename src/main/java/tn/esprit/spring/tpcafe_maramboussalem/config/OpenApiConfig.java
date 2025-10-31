e4ba3be292d8edcc55bbcc3a8af5dfe1deaddaae
maramboussalem <marammaramboussalem@gmail.com>
2025-10-31 18:02:52 +0100

    Amélioration de l'affichage de Swagger dans le projet

--- src/main/java/tn/esprit/spring/tpcafe_maramboussalem/config/OpenApiConfig.java
+++ src/main/java/tn/esprit/spring/tpcafe_maramboussalem/config/OpenApiConfig.java
@@ -1,4 +1,36 @@
 package tn.esprit.spring.tpcafe_maramboussalem.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TPCafe Maram BOUSSALEM")           // NOM DU PROJET
+                        .contact(new Contact()
+                                .name("Linkedin")
+                                .url("https://www.linkedin.com/in/maram-boussalem-3212b9386/")  // LIEN LINKEDIN
+                        )
+                        .license(new License()
+                                .name("GitHub")
+                                .url("https://github.com/maramboussalem")  // LIEN GITHUB
+                        )
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Code source complet")
+                        .url("https://github.com/maramboussalem/spring-boot")     // LIEN GITHUB (encore)
+                );
+    }
 }
+
+

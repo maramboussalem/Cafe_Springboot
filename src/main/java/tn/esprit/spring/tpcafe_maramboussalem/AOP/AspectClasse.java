da72bdf1160349b720f75468d686839d75bc71ba
maramboussalem <marammaramboussalem@gmail.com>
2025-12-10 19:10:49 +0100

    Créer le premier aspect et advices

--- src/main/java/tn/esprit/spring/tpcafe_maramboussalem/AOP/AspectClasse.java
+++ src/main/java/tn/esprit/spring/tpcafe_maramboussalem/AOP/AspectClasse.java
@@ -1,32 +1,72 @@
-package tn.esprit.spring.tpcafe_maramboussalem.aspectes;
+package tn.esprit.spring.tpcafe_maramboussalem.AOP;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-@Component  //pour defenir qu'il sagit  d'un been spring
-@Aspect //pour definir que cette classe est un ascpect
-@Slf4j //pour ecrire des message du logging
+import java.util.Arrays;
+
+@Component
+@Aspect
+@Slf4j
 public class AspectClasse {
-
-
-
 
     @Before("execution(* tn.esprit.spring.tpcafe_maramboussalem.services..*.*(..))")
     public void logMethodeEntry(JoinPoint jp) {
-        log.info("In Methode X" + jp.getSignature().getName());
+        log.info("Hello!" + jp.getSignature().getName());
     }
-
-
 
     @After("execution(* tn.esprit.spring.tpcafe_maramboussalem.services..*.*(..))")
     public void logMethodeEnd(JoinPoint jp) {
-        log.info("In Methode X" + jp.getSignature().getName());
+        log.info("Bye!" + jp.getSignature().getName());
     }
 
+    @Around("execution(* tn.esprit.spring.tpcafe_maramboussalem.services.Client.ClientService.addClient( .. ))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
 
+        Object obj = pjp.proceed();
 
+        long elapsedTime = System.currentTimeMillis() - start;
+
+        log.info("Method execution time: " + elapsedTime + " milliseconds.");
+        return obj;
+    }
+
+    @Around("execution(* tn.esprit.spring.tpcafe_maramboussalem.services.Client.ClientService.*( .. ))")
+    public Object validateInput(ProceedingJoinPoint pjp) throws Throwable {
+
+        Object[] args = pjp.getArgs();
+
+        if (args[0] == null) {
+            log.warn("Impossible de mettre a jour : client null.");
+            return null;
+        }
+            return pjp.proceed();
+    }
+
+    @Around("execution(* tn.esprit.spring.tpcafe_maramboussalem.services ..*.* ( .. ))")
+    public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("Début de la méthode : " + pjp.getSignature());
+        Object result = pjp.proceed();
+        log.info("Fin de la méthode : " + pjp.getSignature());
+        return result;
+
+    }
+
+    @AfterThrowing(pointcut = "execution(* tn.esprit.spring.tpcafe_maramboussalem.services..*(..))", throwing = "ex")
+    public void monitorErrors(JoinPoint jp, Throwable ex) {
+        log.error("Exception dans : " + jp.getSignature().getName());
+        log.error("Message : " + ex.getMessage());
+    }
+
+    @AfterThrowing(pointcut = "execution(* tn.esprit.spring.tpcafe_maramboussalem.services..*(..))", throwing = "ex")
+    public void monitorNullPointer(JoinPoint jp, NullPointerException ex) {
+
+        log.error("NullPointerException dans : " + jp.getSignature().getName());
+        log.error("Message : " + ex.getMessage());
+        log.error("Parametres : " + Arrays.toString(jp.getArgs()));
+    }
 }
